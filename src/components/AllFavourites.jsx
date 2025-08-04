8e6d60ce5ed9e384469e2f7f76d55833cf1ceb0c
spoiltcheese <spoiltcheese@hotmail.com>
2025-08-04 22:39:22 +0800

    scaffloding for viewing favourites

---
+++ src/components/AllFavourites.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllFavourites = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AllFavourites;
---
+++ src/components/Favourite.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Favourite = () => {
+  return <div></div>;
+};
+
+export default Favourite;
